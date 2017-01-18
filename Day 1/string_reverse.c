34a0f53a7b6194a6e69f3ab2223326bf9832d910
Gurkamal Singh <gurkamal2323@gmail.com>
2017-01-19 00:01:37 +0530

    Day 1

--- Day 1/string_reverse.c
+++ Day 1/string_reverse.c
@@ -4,6 +4,7 @@ void printRevString(char *s, int n)
 {
 	if(n<=1)
 		return;
+	//we just hace to reverse the middle characters between first and last character and then swap the two remaining characters i.e first and last
 	printRevString(s+1,n-2);
 	char ch;
 	ch=s[0];
@@ -14,13 +15,16 @@ void printRevString(char *s, int n)
 int main()
 {
 	char s[100];
+	//string read format including spaces
 	scanf("%[^\n]",s);
 	int i,count=0;
+	//count number of characters in string excluding '\0'
 	for(i=0;s[i]!='\0';i++)
 	{
 		count++;
 	}
 	printRevString(s,count);
+	//printing reversed string
 	for(i=0;s[i]!='\0';i++)
 	{
 		printf("%c",s[i]);
